5f35769b4e1c81b36e490904a37da9883336a124
Viet Bui <buiminhviet.2002@gmail.com>
2015-08-15 13:26:11 +0700

    Update XYZPerson.h
    Add some comments!

--- XYZPerson.h
+++ XYZPerson.h
@@ -1,3 +1,9 @@
+//This is one of the exercise in Programming with Objective-C
+//https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/ProgrammingWithObjectiveC.pdf
+//I just try the exercise in here!!
+
+#import <Foundation/Foundation.h>
+
 @interface XYZPerson : NSObject
 @property NSData *firstName;
 @property NSData *lastName;
