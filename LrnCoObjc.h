bb39c5b7c3d099be95de672edc85092802e4348b
Viet Bui <buiminhviet.2002@gmail.com>
2015-08-15 14:59:27 +0700

    Update LrnCoObjc.h

--- LrnCoObjc.h
+++ LrnCoObjc.h
@@ -2,5 +2,11 @@
 #import <Foundation/Foundation.h>
 
 @interface LrnCoObjc :NSObject
+//empty string with this code:
+NSString *aString = [[NSString alloc] init];
+//The following code creates an NSString object, contains the text "Hello, world!":
+NSString *aString = @"Hello, world!";
+//Ask how many letters it has:
+NSInteger sizeOfString = [@"Hello, world!" length];
 
 @end
